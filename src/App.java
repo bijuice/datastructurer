7504ec765a431c0670387364a92e1a9dcc5624fd
bijuice <abdul.r.zaid@gmail.com>
2023-02-18 15:14:18 +0300

    Mystery of the Walrus

--- src/App.java
+++ src/App.java
@@ -1,5 +1,50 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        IntList L = new IntList(15, null);
+        L = new IntList(10, L);
+        L = new IntList(5, L);
+
+        System.out.print(L.get(2));
+    }
+
+}
+
+class IntList {
+    public int first;
+    public IntList rest;
+
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
+
+    public int size() {
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + this.rest.size();
+    }
+
+    public int iterativeSize() {
+        IntList p = this;
+        int totalSize = 0;
+        while (p != null) {
+            totalSize += 1;
+            p = p.rest;
+        }
+        return totalSize;
+    }
+
+    public int get(int i) {
+        IntList p = this;
+        int counter = 0;
+
+        while (counter != i) {
+            p = p.rest;
+            counter += 1;
+        }
+
+        return p.first;
     }
 }
