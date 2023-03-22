c765c46554ad0f8f96cdea1d8f8ef94c07a40563
bijuice <abdul.r.zaid@gmail.com>
2023-03-22 19:13:02 +0300

    helper functions

--- src/App.java
+++ src/App.java
@@ -1,52 +1,91 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        IntList L = new IntList(5, null);
-        L.rest = new IntList(10, null);
-        L.rest.rest = new IntList(15, null);
 
-        System.out.println(L.get(2));
+        SLList L = new SLList(10);
+
+        System.out.println(L.sizeIterative());
+        L.addLastRecursive(5);
+        L.addLastRecursive(10);
+
+        System.out.println(L.sizeIterative());
     }
 
 }
 
-class IntList {
-    public int first;
-    public IntList rest;
+class SLList {
+    static class IntNode {
+        public int item;
+        public IntNode next;
 
-    public IntList(int f, IntList r) {
-        first = f;
-        rest = r;
+        public IntNode(int f, IntNode r) {
+            item = f;
+            next = r;
+        }
+
     }
 
-    public int size() {
-        if (rest == null) {
+    private IntNode first;
+
+    public SLList(int x) {
+        first = new IntNode(x, null);
+    }
+
+    /** Adds an item to the front of the list. */
+    public void addFirst(int x) {
+        first = new IntNode(x, first);
+    }
+
+    /** Retrieves the front item from the list. */
+    public int getFirst() {
+        return first.item;
+    }
+
+    /** Adds an item to the end of the list. */
+    public void addLast(int x) {
+        IntNode p = first;
+
+        /* Advance p to the end of the list. */
+        while (p.next != null) {
+            p = p.next;
+        }
+        p.next = new IntNode(x, null);
+    }
+
+    public static IntNode addLastRecursive(IntNode p) {
+        if (p.next == null) {
+            return p;
+        }
+        return addLastRecursive(p.next);
+    }
+
+    public void addLastRecursive(int x) {
+        IntNode last = addLastRecursive(first);
+
+        last.next = new IntNode(x, null);
+    }
+
+    /** Returns the size of the list starting at IntNode p. */
+    private static int size(IntNode p) {
+        if (p.next == null) {
             return 1;
         }
 
-        return 1 + this.rest.size();
+        return 1 + size(p.next);
     }
 
-    public int iterativeSize() {
+    public int size() {
+        return size(first);
+    }
+
+    public int sizeIterative() {
+        IntNode p = this.first;
         int counter = 0;
-        IntList current = this;
-        while (current != null) {
+        while (p.next != null) {
             counter++;
-            current = current.rest;
+            p = p.next;
         }
 
         return counter;
-    }
-
-    public int get(int i) {
-        int counter = 0;
-        IntList current = this;
-
-        while (i != counter) {
-            counter++;
-            current = current.rest;
-        }
-
-        return current.first;
     }
 }
